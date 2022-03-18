6624c9b36318e2d3f3542bcf61c392aefddaccd1
Andrea <andrea@warnell.in>
2022-03-18 22:14:11 +0530

    Sample react app

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h2>Hello</h2>
+        <img src='docker-icon.png' className="App-logo" alt="logo" />
+        <p>
+          This is my react application.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
